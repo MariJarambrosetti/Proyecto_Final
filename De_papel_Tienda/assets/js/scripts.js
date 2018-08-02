be7c7bd4df35376c92866310f5ffc847e66d178e
mariana <mariana.j.ambrosetti@gmail.com>
2018-08-02 01:50:07 -0400

    instagram feed con margin

--- De_papel_Tienda/assets/js/scripts.js
+++ De_papel_Tienda/assets/js/scripts.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
 	 var userFeed = new Instafeed({
         get: 'user',
         userId: '4937064559',
-        limit: '9',
+        limit: '7',
         accessToken: '4937064559.1677ed0.f024df80ce4441758db21a4970469d64',
         template: '<div class ="col-md"><a href="{{link}}"><img class="img-fluid insta" src="{{image}}"</a></div>'
     });
