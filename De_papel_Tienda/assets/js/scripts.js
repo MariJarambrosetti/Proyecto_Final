42acef9ee3ea2e2bff38dae567bbe212f7fe19d3
mariana <mariana.j.ambrosetti@gmail.com>
2018-08-02 01:33:34 -0400

    instagram feed

--- De_papel_Tienda/assets/js/scripts.js
+++ De_papel_Tienda/assets/js/scripts.js
@@ -1,3 +1,17 @@
 $(document).ready(function() {
+	
+	// instagram
+	
+	 var userFeed = new Instafeed({
+        get: 'user',
+        userId: '4937064559',
+        limit: '9',
+        accessToken: '4937064559.1677ed0.f024df80ce4441758db21a4970469d64',
+        template: '<div class ="col-md"><a href="{{link}}"><img class="img-fluid insta" src="{{image}}"</a></div>'
+    });
+    userFeed.run();
+
+
+
 
 });
