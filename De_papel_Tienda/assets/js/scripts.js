c252f81beca2db4d102243b355c935663631ec46
mariana <mariana.j.ambrosetti@gmail.com>
2018-08-03 19:40:52 -0400

    cambio de imagenes carousel, jquery scroll logo, degradé imagenes

--- De_papel_Tienda/assets/js/scripts.js
+++ De_papel_Tienda/assets/js/scripts.js
@@ -2,16 +2,22 @@ $(document).ready(function() {
 	
 	// instagram
 	
-	 var userFeed = new Instafeed({
-        get: 'user',
-        userId: '4937064559',
-        limit: '7',
-        accessToken: '4937064559.1677ed0.f024df80ce4441758db21a4970469d64',
-        template: '<div class ="col-md"><a href="{{link}}"><img class="img-fluid insta" src="{{image}}"</a></div>'
-    });
-    userFeed.run();
+	var userFeed = new Instafeed({
+		get: 'user',
+		userId: '4937064559',
+		limit: '7',
+		accessToken: '4937064559.1677ed0.f024df80ce4441758db21a4970469d64',
+		template: '<div class ="col-md"><a href="{{link}}"><img class="img-fluid insta" src="{{image}}"</a></div>'
+	});
+	userFeed.run();
 
+	// regla para que cambie de color el logo
 
-
+	$(window).scroll(function(event) {
+		event.preventDefault();
+		if ( $(window).scrollTop() ) {
+			$("#logo").attr('src', 'assets/img/depapel_1.png');
+		} else {$("#logo").attr('src', 'assets/img/depapel.png');}
+	});
 
 });
