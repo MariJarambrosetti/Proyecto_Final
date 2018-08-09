4f77c0fe080fafe0ce8f7f2255bbe6a09880735f
mariana <mariana.j.ambrosetti@gmail.com>
2018-08-09 11:29:52 -0400

    arreglos profe

--- De_papel_Tienda/assets/js/scripts.js
+++ De_papel_Tienda/assets/js/scripts.js
@@ -1,3 +1,6 @@
+var window_size, limit;
+
+
 // API GOOGLE MAPS
 
 	function initMap(){
@@ -10,10 +13,9 @@
 	};
 
 
-$(document).ready(function() {
+(function($) {
 	
 	// instagram
-	
 	var userFeed = new Instafeed({
 		get: 'user',
 		userId: '4937064559',
@@ -21,42 +23,36 @@ $(document).ready(function() {
 		accessToken: '4937064559.1677ed0.f024df80ce4441758db21a4970469d64',
 		template: '<div class ="insta_imagen col-md col-sm"><a href="{{link}}"><img class="img-fluid insta" src="{{image}}"</a></div>'
 	});
+
 	userFeed.run();
 
-	// regla para que cambie de color el logo
+	window_size = $(window).width();
+	limit = $('#carousel_home').outerHeight() - 85;
 
-	$(window).scroll(function(event) {
-		event.preventDefault();
-		if ( $(window).scrollTop() > 300 ) {
-			$("#logo").attr('src', 'assets/img/depapel_1.png');
-		} else {
-			$("#logo").attr('src', 'assets/img/depapel.png');
-		}
-	});
+})(jQuery);
 
-	// regla para el cambio de color del menu
-	
 
-	$(window).resize(function(event) {
-		event.preventDefault();
-		if ($(window).width() > 980) {
+// regla para que cambie de color el logo
+$(window).scroll(function(event) {
+	event.preventDefault();
 
-			// si windows mide más de 980, ejecutará el cambio de color con el scroll
-			$(window).scroll(function(event) {
-				event.preventDefault();
-				if ( $(window).scrollTop() > 300 ) {
-					$(".nav-link").attr('style', 'color :#343a40!important');
-				} else {
-					$(".nav-link").attr('style', 'color :white!important');
-				}
-				
-			});
+	var scroll_position =  $(window).scrollTop();
 
-		} else { 
-			// si windows es menor a 980 se mantendrá en blanco el color 
-			$(".nav-link").attr('style', 'color :white!important');
-		}
-	});
+	if ( scroll_position > limit ) {
+		$("#logo").attr('src', 'assets/img/depapel_1.png');
+	} else {
+		$("#logo").attr('src', 'assets/img/depapel.png');
+	}
+
+
+	if ( scroll_position > limit && window_size > 980 ) {
+		$(".nav-link").attr('style', 'color :#343a40!important');
+	} else {
+		$(".nav-link").attr('style', 'color :white!important');
+	}
+
 });
+
+// regla para el cambio de color del menu
 
 
