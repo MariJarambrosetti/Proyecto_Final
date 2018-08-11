1ae30d10c2f026a0b824abaf0aa0946fc02e68ca
mariana <mariana.j.ambrosetti@gmail.com>
2018-08-11 00:45:36 -0400

    Problema con la carga de CDN

--- De_papel_Tienda/WP-Depapel/wp-content/themes/depapel-theme/functions/wp_enqueue_scripts.php
+++ De_papel_Tienda/WP-Depapel/wp-content/themes/depapel-theme/functions/wp_enqueue_scripts.php
@@ -1,26 +1,41 @@
 <?php 
-	
-	function dl_enqueue_scripts() {
-		$theme_data = wp_get_theme();
 
-		if (!class_exists( 'WooCommerce' )) {
-			/* Deregister Scripts */
-			wp_deregister_script( 'jquery' );
-			wp_deregister_script( 'jquery-migrate' );
+function dl_enqueue_scripts() {
+	$theme_data = wp_get_theme();
 
-			/* Register Scripts */
-			wp_register_script('jquery', get_theme_file_uri('/assets/js/lib/jquery.min.js'), null, '3.3.1', true);
-			wp_register_script('jquery-migrate', get_theme_file_uri('/assets/js/lib/jquery-migrate.min.js'), array('jquery'), '3.0.0', true);
-		}
+	if (!class_exists( 'WooCommerce' )) {
+		/* Deregister Scripts */
+		wp_deregister_script( 'jquery' );
+		wp_deregister_script( 'jquery-migrate' );
 
 		/* Register Scripts */
-		wp_register_script('flickity', get_theme_file_uri('/assets/js/lib/flickity.pkgd.js'), array('jquery-migrate'), '2.1.0', true);
-		wp_register_script('mainJS', get_theme_file_uri('/assets/js/functions.js'), array('jquery-migrate'), $theme_data->get( 'Version' ), true);
-
-		/* Enqueue Scripts */
-		wp_enqueue_script('flickity');
-		wp_enqueue_script('mainJS');
+		wp_register_script('jquery', get_theme_file_uri('/assets/js/lib/jquery.min.js'), null, '3.3.1', true);
+		wp_register_script('jquery-migrate', get_theme_file_uri('/assets/js/lib/jquery-migrate.min.js'), array('jquery'), '3.0.0', true);
 	}
 
-	add_action( 'wp_enqueue_scripts', 'dl_enqueue_scripts' );
+	/* Register Scripts */
+	wp_register_script('jquery', get_theme_file_uri('/assets/js/lib/jquery.min.js'), null, '3.3.1', true);
+
+	wp_register_script('jquery-migrate', get_theme_file_uri('/assets/js/lib/jquery-migrate.min.js'), array('jquery'), '3.0.0', true);
+
+	wp_register_script('flickity', get_theme_file_uri('/assets/js/lib/flickity.pkgd.js'), array('jquery-migrate'), '2.1.0', true);
+
+	wp_register_script('mainJS', get_theme_file_uri('/assets/js/functions.js'), array('jquery-migrate'), $theme_data->get( 'Version' ), true);
+
+	wp_register_script( 'popper', 'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js', null, null, true);
+
+	wp_register_script('bootstrap_js', 'https//stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js',null, null, true );
+
+	/* Enqueue Scripts */
+	wp_enqueue_script('jquery');
+	wp_enqueue_script('jquery-migrate');
+	wp_enqueue_script('flickity');
+	wp_enqueue_script('mainJS');
+
+
+	// wp_enqueue_scripts('popper');
+	// wp_enqueue_scripts('bootstrap_js'); 
+}
+
+add_action( 'wp_enqueue_scripts', 'dl_enqueue_scripts' );
 ?>
