fa1449953c28d853a5c1662bad6c622b29e458cd
mariana <mariana.j.ambrosetti@gmail.com>
2018-08-10 22:43:27 -0400

    Registro Font Awesome

--- De_papel_Tienda/WP-Depapel/wp-content/themes/depapel-theme/functions/wp_enqueue_style.php
+++ De_papel_Tienda/WP-Depapel/wp-content/themes/depapel-theme/functions/wp_enqueue_style.php
@@ -10,6 +10,7 @@
 		wp_register_style('bootstrap_css', get_parent_theme_file_uri( '/assets/css/bootstrap.min.css' ), null, $theme_data->get( 'Version' ), 'screen' );
 		wp_register_style('custom_style',get_parent_theme_file_uri( '/assets/css/custom_style.css' ), null, $theme_data->get('Version'), 'screen' );
 		wp_register_style( 'lato', 'https://fonts.googleapis.com/css?family=Lato:400,700,900', null, $theme_data->get('Version'), 'screen' );
+		wp_register_style( 'fontawesome', 'https://use.fontawesome.com/releases/v5.2.0/css/all.css', null, $theme_data->get('Version'), 'screen' );
 		
 		/* Enqueue Scripts */
 		wp_enqueue_style('flickity');
@@ -17,6 +18,7 @@
 		wp_enqueue_style('bootstrap_css');
 		wp_enqueue_style('custom_style');
 		wp_enqueue_style('lato');
+		wp_enqueue_style('fontawesome');
 	}
 
 	add_action( 'wp_enqueue_scripts', 'dl_enqueue_style' );
