6cd3d9a702768c741eb5d52b23edd12bd9046014
Cristobal Dominguez <cristobald@gmail.com>
2018-08-22 20:51:57 -0300

    Mejora los estilos del menú principal

--- De_papel_Tienda/WP-Depapel/wp-content/themes/depapel-theme/functions/adds_custom_menu_classes.php
+++ De_papel_Tienda/WP-Depapel/wp-content/themes/depapel-theme/functions/adds_custom_menu_classes.php
@@ -1,20 +1,11 @@
 <?php
 
-add_filter( 'nav_menu_link_attributes', 'dl_menu_add_class', 10, 3 );
-
 function dl_menu_add_class( $atts, $item, $args ) {
-	$class = 'nav-link dropdown-toggle';
+
+	$class = 'nav-link';
 	$atts['class'] = $class;
 	return $atts;
+
 }
 
-
-
-
-
-function new_submenu_class($menu) {    
-    $menu = preg_replace('/ class="sub-menu"/','/ class="dropdown-menu" /',$menu);        
-    return $menu;      
-}
-
-add_filter('wp_nav_menu','new_submenu_class');
+add_filter( 'nav_menu_link_attributes', 'dl_menu_add_class', 10, 3 );
--- De_papel_Tienda/WP-Depapel/wp-content/themes/depapel-theme/header.php
+++ De_papel_Tienda/WP-Depapel/wp-content/themes/depapel-theme/header.php
@@ -23,53 +23,10 @@
 			<?php if ( has_nav_menu( 'header-menu' ) ) { ?>
 				<?php wp_nav_menu( array(
 					'theme_location' => 'header-menu',
-					'container' => false,
-					'menu_class'      	=> 'nav navbar-nav ml-auto',
-					'menu_id'         	=> '',
-					'echo'            	=> true,
-					'fallback_cb'     	=> 'wp_page_menu',
-					'items_wrap'      	=> '<ul id="%1$s" class="%2$s">%3$s</ul>',
-					'depth'           	=> 2,
-					'walker'          	=> ''
+					'container' 		=> false,
+					'menu_class'      	=> 'nav navbar-nav ml-auto'
 				)); ?>
 			<?php } ?>
 
-			<!--<ul class="navbar-nav ml-auto">
-				<li class="nav-item dropdown">
-					<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-						Planificadores
-					</a>
-					<div class="dropdown-menu" aria-labelledby="navbarDropdown">
-						<a class="dropdown-item" href="#">Disponibles</a>
-						<a class="dropdown-item" href="#">Personalizados</a>
-						<a class="dropdown-item" href="#">Profesionales</a>
-					</div>
-				</li>
-				<li class="nav-item dropdown">
-					<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-						Eventos
-					</a>
-					<div class="dropdown-menu" aria-labelledby="navbarDropdown">
-						<a class="dropdown-item" href="#">Cumpleaños</a>
-						<a class="dropdown-item" href="#">Bautizos</a>
-						<a class="dropdown-item" href="#">Matrimonio</a>
-					</div>
-				</li>
-				<li class="nav-item">
-					<a class="nav-link" href="#">Servicios Corporativos</a>
-				</li>
-				<li class="nav-item">
-					<a class="nav-link" href="#">Coaching</a>
-				</li>
-				<li class="nav-item">
-					<a class="nav-link" href="#">Contacto</a>
-				</li>
-				<li class="nav-item">
-					<a class="nav-link" href="#"><i class="fas fa-shopping-cart"></i></i></a>
-				</li>
-				<form id="buscar" action="#">
-					<input class="buscando" type="search" placeholder="Search">
-				</form>
-			</ul> -->
 		</div>
 	</nav>
