21b6d72132b6bd65e6c56d9bf86c5994a486b813
mariana <mariana.j.ambrosetti@gmail.com>
2018-08-27 17:46:52 -0300

    Loop Productos + boton a tianda + css responsive

--- De_papel_Tienda/WP-Depapel/wp-content/themes/depapel-theme/home.php
+++ De_papel_Tienda/WP-Depapel/wp-content/themes/depapel-theme/home.php
@@ -56,56 +56,25 @@
 
 		<!-- linea de productos -->
 		<div class="row producto">
-			<!-- producto 1 -->
-			<div class="col-md-3 col-sm-6 producto_div ">
-				<div class="card producto_planificadores">
-					<img class="card-img-top" src="<?php bloginfo('template_url')?>/assets/images/planificador1.jpg" alt="Planificador Disponible">
-					<div class="card-body">
-						<h5 class="card-title">Card title</h5>
-						<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-						<a href="#" class="btn btn-primary">Comprar</a>
-					</div>
-				</div>
-			</div>	
-			<!-- producto 2	 -->
-			<div class="col-md-3 col-sm-6 producto_div ">
-				<div class="card producto_planificadores">
-					<img class="card-img-top" src="<?php bloginfo('template_url')?>/assets/images/planificador2.jpg" alt="Planificador Disponible">
-					<div class="card-body">
-						<h5 class="card-title">Card title</h5>
-						<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-						<a href="#" class="btn btn-primary">Comprar</a>
-					</div>
-				</div>
-			</div>	
-			<!-- producto 3	 -->
-			<div class="col-md-3 col-sm-6 producto_div ">
-				<div class="card producto_planificadores">
-					<img class="card-img-top" src="<?php bloginfo('template_url')?>/assets/images/planificador1.jpg" alt="Planificador Disponible">
-					<div class="card-body">
-						<h5 class="card-title">Card title</h5>
-						<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-						<a href="#" class="btn btn-primary">Comprar</a>
-					</div>
-				</div>
-			</div>	
-			<!-- producto 4 -->
-			<div class="col-md-3 col-sm-6 producto_div ">
-				<div class="card producto_planificadores">
-					<img class="card-img-top" src="<?php bloginfo('template_url')?>/assets/images/planificador2.jpg" alt="Planificador Disponible">
-					<div class="card-body">
-						<h5 class="card-title">Card title</h5>
-						<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-						<a href="#" class="btn btn-primary">Comprar</a>
-					</div>
-				</div>
-			</div>			
-		</div> 
-		<!-- boton VER TODOS los productos -->
-		<div class="container boton_planificadores">
-			<button type="button" class="btn btn-info btn-lg">Ver Todos</button>
+			<!-- productos-->
+			<?php 
+			$args = array( 
+				'post_type' => 'product', 
+				'category' => 'Planificadores',
+				'posts_per_page' => 4 );
+				?>
+
+				<?php $loop = new WP_Query( $args ); ?>
+
+				<?php while ( $loop->have_posts() ) : $loop->the_post(); global $product; ?>
+					<?php woocommerce_get_template_part( 'content', 'product' ); ?>	
+				<?php endwhile; ?>		
+			</div> 
+			<!-- boton VER TODOS los productos -->
+			<div class="container boton_planificadores">
+				<a href="<?php bloginfo('url')?>/shop/" title=""><button type="button" class="btn btn-info btn-lg">Ver Todos</button></a>	
+			</div>
 		</div>
-	</div>
 
 	<!-- eventos especiales  -->
 	<div class="container-fluid eventos_especiales">
