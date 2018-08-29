f143063a5992d78fbb82a23dc71d9f78d8138be6
mariana <mariana.j.ambrosetti@gmail.com>
2018-08-29 17:46:17 -0300

    Page templates, actualizaciones plugins, WC a ES

---
+++ De_papel_Tienda/WP-Depapel/wp-content/themes/depapel-theme/page_template2.php
@@ -0,0 +1,23 @@
+<?php 
+	/*
+		Template Name: 2
+	*/
+?>
+
+<?php get_header() ?>
+
+<?php the_post() ?>
+
+<?php the_post_thumbnail() ?>
+
+<header id="header_1" class="header_1 site-main">
+	<h1><?php the_title();?></h1>
+</header>
+
+<main class="content gallery">
+    <div class="container">
+        <?php the_content(); ?>
+    </div>
+</main>
+
+<?php get_footer() ?>
